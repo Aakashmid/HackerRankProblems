7f8175455ebf133fd4dc9f15af874b661b05c1c6
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-23 22:59:31 +0530

    Use nametuple, find average Marks

--- python/CollectionNametupleUse.py
+++ python/CollectionNametupleUse.py
@@ -2,3 +2,18 @@ from collections import namedtuple
 N=int(input())
 
 ColumnName=input().split()
+
+Data=namedtuple('Marks', ColumnName )
+studentDetails=[]
+for i in range(N):
+    a=input().split()
+    student=Data(a[0],a[1],a[2],a[3])
+    studentDetails.append(student)
+
+
+totalMarks=0
+for st in studentDetails:
+    totalMarks+=int(st.MARKS)
+
+
+print(totalMarks/N)
