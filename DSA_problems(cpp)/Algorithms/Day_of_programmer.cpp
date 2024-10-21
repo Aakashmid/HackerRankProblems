34a59b6dadf7c567149c9a1fb68761676f1f7932
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-10-21 21:29:50 +0530

    updated DSA folder , added new codes

---
+++ DSA_problems(cpp)/Algorithms/Day_of_programmer.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+
+// function to return day of the programmer in given year
+string dayOfProgrammer(int year) {
+    string date="";
+    if(year%4 == 0){
+        date= "12.09."+to_string(year);
+        cout<<date<<endl;
+        return date;
+    }
+        date = "13.09."+to_string(year);
+        cout<<date<<endl;
+        return date;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
