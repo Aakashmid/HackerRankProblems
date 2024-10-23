6530ff082704c52721de1fbe183b0cefe0673a33
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-10-23 23:16:27 +0530

    modified

---
+++ DSAProblem/ArrayManipulation.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+/*
+Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each the array element between two given indices, inclusive. Once all operations have been performed, return the maximum value in the array.
+
+*/
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+
+//////////////////////////////// my code //////////////////////////////////
+long arrayManipulation(int n, vector<vector<int>> queries) {
+    long max=0;
+    vector<long> arr(n,0);
+    for(int i=0;i<queries.size();i++){
+        long a=queries[i][0] , b = queries[i][1] , k = queries[i][2];
+        a--;b--;
+        arr[a]+=k;
+        if ((b+1)<arr.size()) arr[b+1]-=k;  // checking if b+1 in out of range of array length
+    }
+    
+    long curr=0;
+    for (int i=0;i<arr.size();i++) { 
+        // summing all numbers and checking for max at each step 
+         curr+=arr[i];
+         if (max<curr) max=curr;
+    }
+    return max;
+}
+/////////////////////////////////////////////
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> queries(m);
+
+    for (int i = 0; i < m; i++) {
+        queries[i].resize(3);
+
+        string queries_row_temp_temp;
+        getline(cin, queries_row_temp_temp);
+
+        vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int queries_row_item = stoi(queries_row_temp[j]);
+
+            queries[i][j] = queries_row_item;
+        }
+    }
+
+    long result = arrayManipulation(n, queries);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
